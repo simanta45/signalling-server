2e6c3cf6f570d854e38319db9f88c230eb137326
shacker <simantakalita39566@gmail.com>
2024-06-16 21:36:17 +0530

    add another path

--- index.js
+++ index.js
@@ -10,6 +10,10 @@ server.get("/",(req,res)=>{
     res.send("<h1>you are in the home page</h1>") 
 })
 
+server.get("/about",(req,res)=>{
+    res.send("<h1>you are in the about page</h1>") 
+})
+
 server.listen(PORT,()=>{
     console.log("server is runnung in the port"+ PORT)
 })
