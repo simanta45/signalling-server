dbef05cae2ebd2543b01e9cbbae7787e6242ce7a
shacker <simantakalita39566@gmail.com>
2024-06-16 13:36:16 +0530

    second time commit

--- index.js
+++ index.js
@@ -4,8 +4,13 @@ require("dotenv").config()
 const server=express()
 
 const PORT=3000 || process.env.PORT
+const HOST='0.0.0.0'
 
-server.listen(PORT,()=>{
+server.get("/",(req,res)=>{
+    res.send("<h1>you are in the home page</h1>") 
+})
+
+server.listen(PORT,HOST,()=>{
     console.log("server is runnung in the port"+ PORT)
 })
 
